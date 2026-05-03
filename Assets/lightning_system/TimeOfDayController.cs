df956c4273029bab0c1aa85cd2a2638b7aff30a9
dangt0n <dangt0n78@gmail.com>
2026-05-03 13:26:38 +0700

    add moon and samplescene

--- Assets/lightning_system/TimeOfDayController.cs
+++ Assets/lightning_system/TimeOfDayController.cs
@@ -1,35 +1,45 @@
 using UnityEngine;
 using UnityEngine.UI;
 
+[ExecuteAlways]
 public class TimeOfDayController : MonoBehaviour
 {
-    [Header("Sun")]
+    [Header("Object")]
     public GameObject sunObject;
-
-    [Header("Slider")]
+    public GameObject moonObject;
     public Slider timeSlider;
 
-    void Start()
-    {
-        
-        if (timeSlider != null)
-        {
-            timeSlider.onValueChanged.RemoveAllListeners();
-            timeSlider.onValueChanged.AddListener(UpdateTimeOfDay); 
-        }
-    }
+    [Header("Slider test")]
+    [Range(0f, 24f)]
+    public float timeOfDay = 12f;
 
-    public void UpdateTimeOfDay(float timeInHours)
+    private Light sunLight;
+    private Light moonLight;
+
+    void Update()
     {
+        if (Application.isPlaying && timeSlider != null) timeOfDay = timeSlider.value;
+        else if (!Application.isPlaying && timeSlider != null) timeSlider.value = timeOfDay;
+
+        if (sunLight == null && sunObject != null) sunLight = sunObject.GetComponent<Light>();
+        if (moonLight == null && moonObject != null) moonLight = moonObject.GetComponent<Light>();
+
         if (sunObject != null)
         {
-            float sunRotationX = (timeInHours / 24f) * 360f - 90f;
-
-            
+            float sunRotationX = (timeOfDay / 24f) * 360f - 90f;
             sunObject.transform.rotation = Quaternion.Euler(sunRotationX, -30f, 0f);
 
-            
-            Debug.Log("Sun is at: " + sunRotationX + " degrees");
+            if (sunLight != null)
+                sunLight.enabled = (timeOfDay > 6f && timeOfDay < 18f);
+        }
+
+        if (moonObject != null)
+        {
+            float moonRotationX = (timeOfDay / 24f) * 360f - 90f + 180f;
+            moonObject.transform.rotation = Quaternion.Euler(moonRotationX, -60f, 0f);
+
+            if (moonLight != null)
+                moonLight.enabled = (timeOfDay <= 6f || timeOfDay >= 18f);
         }
     }
 }
